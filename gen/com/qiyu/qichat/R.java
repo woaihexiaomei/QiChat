a38ab02672cbbf693adacad8acf17ddf3173e4fc
wan <wan@172.16.21.40>
2016-03-06 12:27:24 +0800

    2

--- gen/com/qiyu/qichat/R.java
+++ gen/com/qiyu/qichat/R.java
@@ -13,8 +13,20 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int et=0x7f060002;
+        public static final int et_name=0x7f060004;
+        public static final int et_psw=0x7f060005;
+        public static final int send=0x7f060003;
+        public static final int tx=0x7f060001;
+        public static final int tx_username=0x7f060000;
+    }
+    public static final class layout {
+        public static final int chat_layout=0x7f030000;
+        public static final int main_layout=0x7f030001;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
@@ -38,10 +50,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
